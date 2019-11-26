7733bda39ee4a527991fd49b72def5dc7f861580
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-11-26 12:41:59 +0300

    add signup validation

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -8,10 +8,10 @@ const app = express();
 admin.initializeApp();
 firebase.initializeApp(config);
 
+const db = admin.firestore();
+
 app.get('/screams', (req, res) => {
-	admin
-		.firestore()
-		.collection('screams')
+	db.collection('screams')
 		.orderBy('createdAt', 'desc')
 		.get()
 		.then(data => {
@@ -36,9 +36,7 @@ app.post('/scream', (req, res) => {
 		likeCount: req.body.likeCount,
 		createdAt: new Date().toISOString()
 	};
-	admin
-		.firestore()
-		.collection('screams')
+	db.collection('screams')
 		.add(newScream)
 		.then(doc => {
 			res.json({ message: `document ${doc.id} created successfully` });
@@ -59,19 +57,46 @@ app.post('/signup', (req, res) => {
 	};
 
 	// TODO: validate data
-
-	firebase
-		.auth()
-		.createUserWithEmailAndPassword(newUser.email, newUser.password)
+	db.doc(`/users/${newUser.handle}`)
+		.get()
+		.then(doc => {
+			if (doc.exists) {
+				return res
+					.status(400)
+					.json({ handle: 'this handle is already taken' });
+			} else {
+				return firebase
+					.auth()
+					.createUserWithEmailAndPassword(
+						newUser.email,
+						newUser.password
+					);
+			}
+		})
 		.then(data => {
-			return res.status(201).json({
-				message: `user ${data.user.uid} signed up successfully`
-			});
+			return data.user.getIdToken();
+		})
+		.then(token => {
+			return res.status(201).json({ token });
 		})
 		.catch(err => {
 			console.error(err);
 			return res.status(500).json({ error: err.code });
 		});
 });
+
+// 	firebase
+// 		.auth()
+// 		.createUserWithEmailAndPassword(newUser.email, newUser.password)
+// 		.then(data => {
+// 			return res.status(201).json({
+// 				message: `user ${data.user.uid} signed up successfully`
+// 			});
+// 		})
+// 		.catch(err => {
+// 			console.error(err);
+// 			return res.status(500).json({ error: err.code });
+// 		});
+// });
 
 exports.api = functions.https.onRequest(app);
