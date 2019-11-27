c8010f8260180b5e54ce5e7e8629f985ec898a5b
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-11-27 14:23:38 +0300

    add middleware to protect routes

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -24,8 +24,42 @@ app.get('/screams', (req, res) => {
 		.catch(err => console.error(err));
 });
 
+//TODO: Create Firebase Authentication Middleware
+const FBAuth = (req, res, next) => {
+	let idToken;
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer ')
+	) {
+		idToken - req.headers.authorization.split('Bearer ')[1];
+	} else {
+		console.error('No token found');
+		return res.status(403).json({ error: 'Unauthorized' });
+	}
+	admin
+		.auth()
+		.verifyIdToken(idToken)
+		.then(decodedToken => {
+			req.user = decodedToken;
+			console.log(decodedToken);
+			return db
+				.collection('users')
+				.where('userId', '==', req.user.uid)
+				.limit(1)
+				.get();
+		})
+		.then(data => {
+			req.user.handle = data.docs[0].data().handle;
+			return next();
+		})
+		.catch(err => {
+			console.error('Error while verifying token', err);
+			return res.status(403).json(err);
+		});
+};
+
 //Create scream
-app.post('/scream', (req, res) => {
+app.post('/scream', FBAuth, (req, res) => {
 	if (req.method !== 'POST') {
 		return res.status(400).json({ error: 'Method Not Allowed' });
 	}
