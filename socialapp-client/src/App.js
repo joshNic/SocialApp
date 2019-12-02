304c8423f8df5104b35a46f3e245c8730a555ea3
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-12-02 12:12:38 +0300

    add react client

---
+++ socialapp-client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Navbar from './components/Navbar';
+
+function App() {
+	return (
+		<div className='App'>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/signup' component={Signup} />
+				</Switch>
+			</Router>
+		</div>
+	);
+}
+
+export default App;
