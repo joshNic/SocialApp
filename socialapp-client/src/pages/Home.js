7ccf13169279c4358bed7eb02ef4804123d06379
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-12-04 14:02:54 +0300

    add scream component

--- socialapp-client/src/pages/Home.js
+++ socialapp-client/src/pages/Home.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
 class Home extends Component {
+	state = {
+		screams: null
+	};
+	componentDidMount() {
+		axios
+			.get('/screams')
+			.then(res => {
+				console.log(res.data);
+				this.setState({ screams: res.data });
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+
 	render() {
+		let recentScreamMarkup = this.state.screams ? (
+			this.state.screams.map(scream => <p>{scream.body}</p>)
+		) : (
+			<p>Loading</p>
+		);
 		return (
 			<Grid container>
 				<Grid item sm={8} xs={12}>
-					<p>Content....</p>
+					{recentScreamMarkup}
 				</Grid>
 				<Grid item sm={4} xs={12}>
 					<p>profile....</p>
