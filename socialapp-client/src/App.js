78d3d849a024086543087013ebc95bc06ee7a761
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-12-06 15:47:53 +0300

    add redux work flow to login

--- socialapp-client/src/App.js
+++ socialapp-client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import jwtDecode from 'jwt-decode';
+import { Provider } from 'react-redux';
 
 import './App.css';
 import Home from './pages/Home';
@@ -11,6 +12,7 @@ import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 import themeFile from './util/theme';
 import AuthRoute from './util/AuthRoute';
+import store from './redux/store';
 
 const theme = createMuiTheme(themeFile);
 
@@ -29,30 +31,32 @@ if (token) {
 function App() {
 	return (
 		<MuiThemeProvider theme={theme}>
-			<div className='App'>
-				<Router>
-					<Navbar />
-					<div className='container'>
-						<Switch>
-							<Route exact path='/' component={Home} />
-							<AuthRoute
-								exact
-								path='/login'
-								component={Login}
-								authenticated={authenticated}
-							/>
-							<AuthRoute
-								exact
-								path='/signup'
-								component={Signup}
-								authenticated={authenticated}
-							/>
-							{/* <Route exact path='/login' component={Login} />
+			<Provider store={store}>
+				<div className='App'>
+					<Router>
+						<Navbar />
+						<div className='container'>
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<AuthRoute
+									exact
+									path='/login'
+									component={Login}
+									authenticated={authenticated}
+								/>
+								<AuthRoute
+									exact
+									path='/signup'
+									component={Signup}
+									authenticated={authenticated}
+								/>
+								{/* <Route exact path='/login' component={Login} />
 							<Route exact path='/signup' component={Signup} /> */}
-						</Switch>
-					</div>
-				</Router>
-			</div>
+							</Switch>
+						</div>
+					</Router>
+				</div>
+			</Provider>
 		</MuiThemeProvider>
 	);
 }
--- socialapp-client/src/pages/Login.js
+++ socialapp-client/src/pages/Login.js
@@ -7,8 +7,9 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import axios from 'axios';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions';
 
 const styles = theme => ({
 	...theme.custom
@@ -25,6 +26,17 @@ class Login extends Component {
 	//   super()
 	//   this.state
 	// }
+	// UNSAFE_componentWillReceiveProps(nextProps) {
+	// 	if (nextProps.UI.errors) {
+	// 		this.setState({ errors: nextProps.UI.errors });
+	// 	}
+	// }
+	// static static getDerivedStateFromProps(prevProps, prevState) {
+	//   if(prevProps.UI.errors !==prevState.UI.errors){
+	//     return {errors:prevProps.UI.errors}
+	//   }
+
+	// }
 	handleSubmit = event => {
 		event.preventDefault();
 		this.setState({
@@ -34,22 +46,7 @@ class Login extends Component {
 			email: this.state.email,
 			password: this.state.password
 		};
-		axios
-			.post('/login', userData)
-			.then(res => {
-				console.log(res.data);
-				localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-				this.setState({
-					loading: false
-				});
-				this.props.history.push('/');
-			})
-			.catch(err => {
-				this.setState({
-					errors: err.response.data,
-					loading: false
-				});
-			});
+		this.props.loginUser(userData, this.props.history);
 	};
 	handleChange = event => {
 		event.preventDefault();
@@ -58,8 +55,12 @@ class Login extends Component {
 		});
 	};
 	render() {
-		const { classes } = this.props;
-		const { errors, loading } = this.state;
+		// console.log('propssss', this.props);
+		const {
+			classes,
+			UI: { loading }
+		} = this.props;
+		const { errors } = this.state;
 		return (
 			<Grid container className={classes.form}>
 				<Grid item sm />
@@ -129,6 +130,21 @@ class Login extends Component {
 	}
 }
 Login.propTypes = {
-	classes: PropTypes.object.isRequired
+	classes: PropTypes.object.isRequired,
+	loginUser: PropTypes.func.isRequired,
+	user: PropTypes.object.isRequired,
+	UI: PropTypes.object.isRequired
 };
-export default withStyles(styles)(Login);
+const mapStateToProps = state => ({
+	user: state.user,
+	UI: state.UI
+});
+
+const mapActionsToProps = {
+	loginUser
+};
+
+export default connect(
+	mapStateToProps,
+	mapActionsToProps
+)(withStyles(styles)(Login));
